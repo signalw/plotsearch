0faad5a46a258a92200f368dfdcae9d5c451779c
signalw <xinhao@brandeis.edu>
2017-04-21 21:54:43 -0400

    added scraped corpus

--- create_corpus.py
+++ create_corpus.py
@@ -1,13 +1,36 @@
-import json, requests
-import temp # place where raw plot data are stored
+from bs4 import BeautifulSoup
+import json, re, requests
 
-corpus = {}
-for title, plot in temp.data:
-    meta = requests.get("http://www.omdbapi.com/?t=%s" % title).json()
-    if meta["Response"] == "False": continue
+cands, corpus = [], {}
+
+r = requests.get("http://www.themoviespoiler.com/Pages/Spoilers.html").text
+soup = BeautifulSoup(r, "html.parser")
+tables = soup.find_all("table")
+for table in tables:
+    links = table.find_all("a")
+    for link in links:
+        if re.match(r"\.\.\/Spoilers\/\w+\.html", link["href"]):
+            url = "http://www.themoviespoiler.com%s" % link["href"].strip(".")
+            title = link.text
+            if title.endswith(", The"): title = "The "+title[:-5]
+            cands.append((url, title))
+
+for url, title in cands:
+    r = requests.get(url).text
+    soup = BeautifulSoup(r, "html.parser")
+    paragraphs = soup.find_all("p")
+    plot = '\n'.join([p.text for p in paragraphs])
+    try:
+        meta = requests.get("http://www.omdbapi.com/?t=%s" % title).json()
+        assert meta["Response"] == "True"
+    except:
+        print "Fail to get metadata for %s" % title
+        continue
     imdbID = meta["imdbID"]
     meta["Plot"] = plot
     corpus[imdbID] = meta
 
-with open("data/toy_corpus.json", "w") as f:
+print "%d movie plots collected." % len(corpus)
+
+with open("data/movie_corpus.json", "w") as f:
     json.dump(corpus, f, indent=4)
